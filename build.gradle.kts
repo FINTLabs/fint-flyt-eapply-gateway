41f7f15c8d14672c6a93f81263ce297e373736d8
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-07-15 12:50:54 +0200

    feat: oppgrader gateway-starter til 4.0.0-rc-1

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,8 +38,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.17")
 
-    implementation("no.novari:flyt-gateway-starter:3.0.0")
-    implementation("no.novari:flyt-web-resource-server:4.0.0-rc-1")
+    implementation("no.novari:flyt-gateway-starter:4.0.0-rc-1")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
