97fa9d7d993bddfc553b177633a98687d86991a6
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-09-21 15:28:29 +0200

    ikke bump kotlin til 2.4.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,8 @@ buildscript {
 plugins {
     id("org.springframework.boot") version "3.5.16"
     id("io.spring.dependency-management") version "1.1.7"
-    kotlin("jvm") version "2.4.20"
-    kotlin("plugin.spring") version "2.4.20"
+    kotlin("jvm") version "2.4.10"
+    kotlin("plugin.spring") version "2.4.10"
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
     id("io.github.ben-manes.versions") version "0.64.0"
 }
